c116f024f76dd243906bec2f1033cd1a3c16e84c
jia-825 <U1333140@o365.nuu.edu.tw>
2026-03-04 10:08:57 +0800

    測試

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
+<!DOCTYPE html>
+<html lang="zh-TW">
+<head>
+    <meta charset="UTF-8">
+    <title>這是一個簡易的 HTML 頁面</title>
+</head>
+<body>
+    <h1>動物森友會</h1>
+    <p>這裡可以放任何靜態內容，例如文字、圖片或連結。</p>
+    <h2>採取匿名方式與全球人們成為筆友</h2>
+</body>
+</html>
 
+
+
+ 
